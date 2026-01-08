db72c125499b55e507fda0929ecb59d5b4d59708
SaiTeja-0104 <saitejak.004@gmail.com>
2026-01-08 13:39:42 +0530

    corsFix

--- Server/server.js
+++ Server/server.js
@@ -9,9 +9,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173", 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 const server = http.createServer(app);
 const io = new Server(server, {
