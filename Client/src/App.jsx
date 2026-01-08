1079a638808e4e6adae28e739ccda2990f37f903
SaiTeja-0104 <saitejak.004@gmail.com>
2026-01-08 16:25:06 +0530

    vite_fix

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,6 @@ import Ngo from './pages/Ngo'
 import Donor from './pages/Donor'
 import ProtectedRoute from './components/ProtectedRoute'
 import ScrollToTop from './components/ScrollToTop'
-import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   const loc = useLocation();
@@ -20,7 +19,6 @@ const App = () => {
     <div>
       <Toaster />
       <ScrollToTop />
-      <Analytics />
       {show && <Navbar />}
       <Routes>
         <Route path='/' element={<Home />} />
