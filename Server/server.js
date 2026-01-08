957db41a1e89c0c01a7ddf82d486f05946b00313
SaiTeja-0104 <saitejak.004@gmail.com>
2026-01-08 14:08:57 +0530

    cors-fixes

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const corsOptions = {
   origin: [
     "http://localhost:5173", 
-    "https://replate-mauve.vercel.app/"
+    "https://replate-mauve.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
@@ -28,7 +28,7 @@ const io = new Server(server, {
   cors: {
     origin: [
     "http://localhost:5173", 
-    "https://replate-mauve.vercel.app/"
+    "https://replate-mauve.vercel.app"
     ],
     methods: ["GET", "POST"],
   }
